c3d8a37e188436eafe71b2d730a4c4655150fcdc
vera0708 <verasdee@gmail.com>
2023-06-22 15:38:20 +0700

    feat: the 2-d day of intansive - made the forecast for next four days

--- scripts/modules/utils.js
+++ scripts/modules/utils.js
@@ -42,3 +42,79 @@ export const getcurrentDataTime = () => {
         dayOfWeek,
     }
 };
+
+export const getWindDirection = (deg) => {
+    const directions = [
+        '&#8593;',
+        '&#8598;',
+        '&#8592;',
+        '&#8601;',
+        '&#8595;',
+        '&#8600;',
+        '&#8594;',
+        '&#8599;',
+    ];
+    // https://symbl.cc/ru/unicode/blocks/arrows/
+    const i = Math.round(deg / 45) % 8;
+    return directions[i];
+};
+
+export const getDewPoint = (temp, humidity) => {
+    const a = 17.27;
+    const b = 237.3;
+    const addfunc = a * ((temp - 273.15)) / (b + (temp - 273.15)) + Math.log((humidity) / 100);
+    const dewPoint = b * addfunc / (a - addfunc);
+    return dewPoint.toFixed(1);
+};
+
+export const convertPressure = (pressure) => {
+    const mmHg = pressure * 0.750063755419211;
+    return mmHg.toFixed(2);
+};
+
+export const getWeatherForecastData = (data) => {
+    const forecast = data.list.filter(
+        (item) => {
+            return new Date(item.dt_txt).getHours() === 12 &&
+                new Date(item.dt_txt).getDate() > new Date().getDate();
+        });
+
+    const forecastData = forecast.map((item) => {
+        const date = new Date(item.dt_txt);
+        const weekdaysShot = [
+            'вск',
+            'пон',
+            'вт',
+            'ср',
+            'чет',
+            'пят',
+            'суб',
+        ];
+
+        const dayOfWeek = weekdaysShot[date.getDay()];
+        const weatherIcon = item.weather[0].icon;
+
+        let minTemp = Infinity;
+        let maxTemp = -Infinity;
+
+        for (let i = 0; i < data.list.length; i++) {
+            const temp = data.list[i].main.temp;
+            const tempDate = new Date(data.list[i].dt_txt);
+            if (tempDate.getDate() === date.getDate()) {
+                if (temp < minTemp) {
+                    minTemp = temp;
+                } else {
+                    maxTemp = temp;
+                }
+            };
+        }
+        return {
+            dayOfWeek,
+            weatherIcon,
+            minTemp,
+            maxTemp,
+        };
+    });
+    console.log(forecast);
+    return forecastData;
+}
--- scripts/modules/widgetService.js
+++ scripts/modules/widgetService.js
@@ -1,21 +1,27 @@
-import { fetchWeather } from "./APIService.js";
+import { fetchForecast, fetchWeather } from "./APIService.js";
 import { renderWidgetForecast, renderWidgetOther, renderWidgetToday } from "./render.js";
 
 
 export const startWidget = async () => {
+
+    const city = 'Томск';
     const widget = document.createElement('div');
     widget.classList.add('widget');
 
-    const dataWeather = await fetchWeather('Томск');
+    const dataWeather = await fetchWeather(city);
 
     if (dataWeather.success) {
         renderWidgetToday(widget, dataWeather.data);
         renderWidgetOther(widget, dataWeather.data);
     } else {
-        showError()
+        showError(dataWeather.error)
     }
-    console.log(dataWeather)
-    renderWidgetForecast(widget);
 
+    const dataForecast = await fetchForecast(city);
+    if (dataForecast.success) {
+        renderWidgetForecast(widget, dataForecast.data);
+    } else {
+        showError(dataForecast.error)
+    }
     return widget;
 }
